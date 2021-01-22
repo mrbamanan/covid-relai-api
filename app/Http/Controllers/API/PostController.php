bc15d033544086241e9fdab22341582a78e9b30f
Abdoul Hamid <mrbamanan@gmail.com>
2021-01-22 22:00:13 +0100

    Fixing posts

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
     {
         $posts = Post::with(['files', 'author',]);
 
-        if (empty(\auth()->user())) {
+        if (empty(auth()->user())) {
             $posts = Post::with(['files', 'author'])->where('active', '=', true);
         }
 
